92c2302a21b71f2ab949d80fd85bd219e2afcc93
Eugene Dudkin <ZheDre1N@gmail.com>
2024-01-18 20:26:40 +0300

    Initial Commit

---
+++ MetalTutorial/MetalTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetalTutorialApp.swift
+//  MetalTutorial
+//
+//  Created by Eugene Dudkin on 18.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct MetalTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
